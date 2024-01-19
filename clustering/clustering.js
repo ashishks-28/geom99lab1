c7333bbfe93b9164fee119ad381385d1e2cb8fe3
Ashish Kumar Singh <146376272+ashishks-28@users.noreply.github.com>
2024-01-19 11:20:54 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,6 +1,6 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 5,
     center: { lat:48.646, lng:-85.419 }, 
   });
   // Create an array of alphabetical characters used to label the markers.
